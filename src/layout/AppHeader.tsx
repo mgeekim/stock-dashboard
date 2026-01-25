32f160e211a29cb46032757be78629b815f448a7
mgeekim <64mingn@gmail.com>
2026-01-25 21:07:41 +0900

    feat: remove notification dropdown from header
    - Deleted NotificationDropdown component and removed it from AppHeader.

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ThemeToggleButton } from "@/components/common/ThemeToggleButton";
-import NotificationDropdown from "@/components/header/NotificationDropdown";
 import { useSidebar } from "@/context/SidebarContext";
 import Image from "next/image";
 import Link from "next/link";
@@ -163,9 +162,6 @@ const AppHeader: React.FC = () => {
             {/* <!-- Dark Mode Toggler --> */}
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
-
-           <NotificationDropdown />
-            {/* <!-- Notification Menu Area --> */}
           </div>
         </div>
       </div>
